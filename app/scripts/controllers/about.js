796b78cf8db267aed996abd1886513c9acc8bd28
Sterling Stokes <sterlingstokes@gmail.com>
2016-07-10 15:40:51 -0400

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blogzApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the blogzApp
+ */
+angular.module('blogzApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blogzApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the blogzApp
+ */
+angular.module('blogzApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
